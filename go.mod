1f8cd69beaf9f6a7b9c6fe43c81327ed1f35c78d
Voidkai <825839697@qq.com>
2024-07-31 15:22:17 +0800

    update

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module poseidon2
 go 1.21.6
 
 require github.com/iden3/go-iden3-crypto v0.0.16
-
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
@@ -12,5 +11,5 @@ require (
 
 require (
 	github.com/stretchr/testify v1.9.0
-	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 )
--- poseidon2_test.go
+++ poseidon2_test.go
@@ -10,51 +10,35 @@ import (
 )
 
 func TestPoseidonHashFixed(t *testing.T) {
-	b := make([]*ff.Element, 16)
-	b[0] = from_hex("0x00")
-	b[1] = from_hex("0x01")
-	b[2] = from_hex("0x02")
+	// b0 := big.NewInt(0)
+	b1 := big.NewInt(1)
+	b2 := big.NewInt(2)
 
-	poseidon2 := NewPoseidon2(RoundConstants)
-
-	assert.Equal(t, from_hex("0x00"), b[0])
-	assert.Equal(t, from_hex("0x01"), b[1])
-	assert.Equal(t, from_hex("0x02"), b[2])
-
-	h := poseidon2.Permutation([]*ff.Element{b[0], b[1], b[2]})
+	h, _ := HashFixed([]*big.Int{b1, b2})
 	assert.Equal(t,
-		from_hex("0x0bb61d24daca55eebcb1929a82650f328134334da98ea4f847f760054f4a3033"),
-		h[0])
-	assert.Equal(t,
-		from_hex("0x303b6f7c86d043bfcbcc80214f26a30277a15d3f74ca654992defe7ff8d03570"),
-		h[1])
-	assert.Equal(t,
-		from_hex("0x1ed25194542b12eef8617361c3ba7c52e660b145994427cc86296242cf766ec8"),
-		h[2])
+		"5297208644449048816064511434384511824916970985131888684874823260532015509555",
+		h.String())
 }
 
 func TestSbox(t *testing.T) {
-	poseidon2 := NewPoseidon2(RoundConstants)
 	b := ff.NewElement().SetBigInt(big.NewInt(2))
 	assert.Equal(t,
 		"2",
 		b.String())
-	b = poseidon2.SboxP(b)
+	b = SboxP(b)
 	assert.Equal(t,
 		"32",
 		b.String())
 }
 
 func BenchmarkPoseidon2Hash(b *testing.B) {
-	b0 := ff.NewElement().SetBigInt(big.NewInt(0))
-	b1 := ff.NewElement().SetBigInt(utils.NewIntFromString("12242166908188651009877250812424843524687801523336557272219921456462821518061")) //nolint:lll
-	b2 := ff.NewElement().SetBigInt(utils.NewIntFromString("12242166908188651009877250812424843524687801523336557272219921456462821518061")) //nolint:lll
+	b0 := big.NewInt(0)
+	b1 := utils.NewIntFromString("12242166908188651009877250812424843524687801523336557272219921456462821518061") //nolint:lll
+	b2 := utils.NewIntFromString("12242166908188651009877250812424843524687801523336557272219921456462821518061") //nolint:lll
 
-	poseidon2 := NewPoseidon2(RoundConstants)
-
-	bigArray4 := []*ff.Element{b1, b2, b0}
+	bigArray3 := []*big.Int{b1, b2}
 
 	for i := 0; i < b.N; i++ {
-		poseidon2.Permutation(bigArray4) //nolint:errcheck,gosec
+		HashFixedWithDomain(bigArray3, b0) //nolint:errcheck,gosec
 	}
 }
